843a9fd07f252502c479ffae3bc8caf6326be7ff
WilsonVinson <cprewilsonvinson@gmail.com>
2024-07-26 14:59:31 +0800

    main

---
+++ project/scripts/resource/structure.gd
@@ -0,0 +1,9 @@
+
+extends Resource
+class_name Structure
+
+@export_subgroup("Model")
+@export var model:PackedScene # Model of the structure
+
+@export_subgroup("Data")
+@export var price:int # Price of the structure when building
